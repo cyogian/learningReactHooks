1961c4974c557d9da13321ec16a1e259f3957ebd
Amar Nath Yogi <contact@cyogian.dev>
2021-06-25 03:49:58 +0530

    added ContextHooks Container

--- src/containers/ContextHooks.js
+++ src/containers/ContextHooks.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ComponentC from "../components/Context/ComponentC";
 
 const ContextHooks = () => {
   document.title = "Context Hooks - Learning React Hooks";
   return (
     <div>
       <h1>Context Hooks</h1>
+      <ComponentC />
     </div>
   );
 };
